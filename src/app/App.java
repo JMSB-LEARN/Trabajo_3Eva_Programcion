d50796c4fdd27d518621404817a36bc3f0bbed1d
JSMB-LEARN <jmarquess04@iesalbarregas.es>
2025-03-24 17:20:05 +0100

    Test 2

--- src/app/App.java
+++ src/app/App.java
@@ -1,7 +1,39 @@
 package app;
+
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
 public class App {
 
-	public static void main (String[] args){
+	public static void main(String[] args) {
+		System.out.println("J");
 
 	}
+
+	private boolean crearFicheroArchivo(String nombre, Path path, TipoArchivo tipo) {
+		try {
+			Path pathArchivo = path.resolve(nombre);
+
+			switch (tipo) {
+
+			case TipoArchivo.Archivo -> {Files.createFile(pathArchivo);}
+			case TipoArchivo.Directorio -> {Files.createDirectory(pathArchivo);}
+			}
+
+		} catch (Exception e) {
+
+		}
+		return false;
+	}
+
+	private boolean crearFicheroArchivo(String nombre, TipoArchivo tipo) {
+		try {
+			return crearFicheroArchivo(nombre, Paths.get(""), tipo);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return false;
+	}
+
 }
---
+++ src/app/TipoArchivo.java
@@ -0,0 +1,4 @@
+package app;
+public enum TipoArchivo{
+	Archivo,Directorio
+}
